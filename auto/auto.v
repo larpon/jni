5b22c29532dff56a62fba21b630b1af1c32ad5f3
lmp <dev.larpon@gmail.com>
2021-02-12 11:37:36 +0100

    auto: add throw_exception

--- auto/auto.v
+++ auto/auto.v
@@ -11,3 +11,8 @@ pub fn call_object_method(obj jni.JavaObject, signature string, args ...jni.Type
 	env := jni.auto_env()
 	return jni.call_object_method(env, obj, signature, ...args)
 }
+
+pub fn throw_exception(msg string) {
+	env := jni.auto_env()
+	jni.throw_exception(env, msg)
+}
